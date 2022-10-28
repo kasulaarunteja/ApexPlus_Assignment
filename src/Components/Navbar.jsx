9472cdb6e00ef87a97e9630d83f373cdc0a79083
kasulaarunteja <kasulaarunteja@gmail.com>
2022-10-28 12:55:49 +0530

    Navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,16 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <h4>NavBar setup</h4>
-    </div>
+    <div className="sidebar">
+  <Link class="active" to={"/Home"}>Home</Link>
+  <Link to={"/AddScenario"}>Add Scenario</Link>
+  <Link to={"/AllScenario"}>AllScenario</Link>
+  <Link to={"/AddVehicle"}>AddVehicle</Link>
+</div>
   );
 }
 
