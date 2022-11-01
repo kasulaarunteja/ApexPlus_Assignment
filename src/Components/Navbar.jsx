b52b1ec6afd505d89a6959eda7719fc3c1c18b24
kasulaarunteja <kasulaarunteja@gmail.com>
2022-11-01 20:26:32 +0530

    navb

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <div className="sidebar">
-  <Link class="active" to={"/Home"}>Home</Link>
+  <Link className="active" to={"/Home"}>Home</Link>
   <Link to={"/AddScenario"}>Add Scenario</Link>
   <Link to={"/AllScenario"}>AllScenario</Link>
   <Link to={"/AddVehicle"}>AddVehicle</Link>
