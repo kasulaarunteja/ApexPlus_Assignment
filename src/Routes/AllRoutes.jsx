c4e310ba3790acceb2f52e89e30126160c457b17
kasulaarunteja <kasulaarunteja@gmail.com>
2022-11-01 20:26:36 +0530

    allr

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 
 import React from "react";
 import { Routes,Route } from "react-router-dom";
+import AddScenario from "../Components/AddScenario";
 import Navbar from "../Components/Navbar";
 
 const AllRoutes = () => {
     return(
         <Routes>
            <Route path="/" element={<Navbar/>}/>
+           <Route path="/AddScenario" element={<AddScenario/>}/>
         </Routes>
     )
 }
